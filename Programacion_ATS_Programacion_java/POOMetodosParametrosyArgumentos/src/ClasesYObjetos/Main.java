ae28556d8fe7eec0bd86bb27a21101b854d82e02
dbustamantep <vtdanielf@gmail.com>
2023-07-14 19:28:09 -0600

    realizamos los comentarios del programa

--- Programacion_ATS_Programacion_java/POOMetodosParametrosyArgumentos/src/ClasesYObjetos/Main.java
+++ Programacion_ATS_Programacion_java/POOMetodosParametrosyArgumentos/src/ClasesYObjetos/Main.java
@@ -9,19 +9,22 @@ import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
+        //vatiables locales
         int  n1,n2;
         
+        //pedimos los valores de las variables con JOptionPane
         n1=Integer.parseInt(JOptionPane.showInputDialog("Digite el primer numero"));
         n2=Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo numero"));
         
+        //instanciamos un objeto de nuestra clase operacion
         Operacion op = new Operacion();
         
-        //op.leerNumeros();
+        //mandamos por parametros nuestras variables gloables a los metodos que tenemos en nuestra clase operacion
         op.sumar(n1,n2);
         op.restar(n1,n2);
         op.multiplicar(n1,n2);
         op.dividir(n1,n2);
-        
+        //mostramos los resultados
         op.mostrarResultados();
 
     }
--- Programacion_ATS_Programacion_java/POOMetodosParametrosyArgumentos/src/ClasesYObjetos/Operacion.java
+++ Programacion_ATS_Programacion_java/POOMetodosParametrosyArgumentos/src/ClasesYObjetos/Operacion.java
@@ -18,25 +18,30 @@ public class Operacion {
     //metodos
     
     //metodo para sumar ambos numeros
+    //metdo que recibe dos parametros y no retorna
     public void sumar(int numero1,int numero2) {
         suma=numero1+numero2;
     }
 
     //metodo para restar ambos numeros
+    //metdo que recibe dos parametros y no retorna
     public void restar(int numero1,int numero2) {
         resta=numero1-numero2;
     }
 
     //metodo para multiplicar ambos numeros
+    //metdo que recibe dos parametros y no retorna
     public void multiplicar(int numero1,int numero2) {
         multiplicacion=numero1*numero2;
     }
 
     //metodo para dividir ambos numeros
+    //metdo que recibe dos parametros y no retorna
     public void dividir(int numero1,int numero2) {
         division=numero1/numero2;
     }
 
+    //mostramos los resultados
     public void mostrarResultados() {
         System.out.println("La suma es " + suma);
         System.out.println("La resta es " + resta);
