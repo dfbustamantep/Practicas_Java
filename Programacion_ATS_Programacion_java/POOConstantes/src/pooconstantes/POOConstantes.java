1a367a780f456bb2cf48be89b0a7f5fb926a64a4
dfbustamantep <vtdanielf@gmail.com>
2023-08-03 18:38:35 -0500

    Constantes Programacion ATS video 69

---
+++ Programacion_ATS_Programacion_java/POOConstantes/src/pooconstantes/POOConstantes.java
@@ -0,0 +1,17 @@
+
+package pooconstantes;
+
+/**
+ *
+ * @author DBustamanteP
+ */
+public class POOConstantes {
+
+    public static void main(String[] args) {
+        Persona persona1 =new Persona("Daniel",18);
+        
+        persona1.setEdad(19);
+        persona1.mostrarDatos();
+    }
+    
+}
