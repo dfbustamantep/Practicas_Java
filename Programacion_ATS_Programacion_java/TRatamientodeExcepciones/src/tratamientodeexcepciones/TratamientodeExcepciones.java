af1780fa44f5e680ce460dfcaf75929f24132d16
dfbustamantep <vtdanielf@gmail.com>
2023-08-23 21:55:06 -0500

    Excepciones en java Programacion ATS video 82

---
+++ Programacion_ATS_Programacion_java/TRatamientodeExcepciones/src/tratamientodeexcepciones/TratamientodeExcepciones.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tratamientodeexcepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DBustamanteP
+ */
+public class TratamientodeExcepciones {
+
+    public static void main(String[] args) {
+        /*
+        java.lang.ArithmeticException excepcion genrada cuando dividimos un numero entre 0
+        int n1=5,n2=0;
+        
+        int resultado=n1/n2;
+        
+        System.out.println("El resultado es "+resultado);*/
+        
+        /* java.util.InputMismatchException excepcion generada cuando insertamosun dato que no corresponde
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digite un numero");
+        int numero = entrada.nextInt();*/
+    }
+    
+}
