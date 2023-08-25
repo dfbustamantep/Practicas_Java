725a80ba78c7499bfc59bf339eab3877968bcf8f
dfbustamantep <vtdanielf@gmail.com>
2023-08-24 23:35:00 -0500

    Paneles - Creacion de un panel Programacion ATS vdieo 89

--- Programacion_ATS_Programacion_java/Ventanas/src/Ventana/Ventana.java
+++ Programacion_ATS_Programacion_java/Ventanas/src/Ventana/Ventana.java
@@ -1,7 +1,10 @@
 
 package Ventana;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Ventana extends JFrame{
     public Ventana(){
@@ -11,6 +14,20 @@ public class Ventana extends JFrame{
         //setLocation(100,200);//Coordenada X Coordenada Y Establecemos la posicion inicla de la vantana
         //setBounds(100, 200, 500, 500);//setLocation y setSize en un mismo metodo, primeros parametros las cooredenas  X y Y,los otors dos son el tamaño de la ventana
         setLocationRelativeTo(null);//null establecemos en el centro de la pantalla
+        
+        
+        //setResizable(false);//Establecemos si la ventana se puede cmabiar de tamaño o no
+        setMinimumSize(new Dimension(200,100));//Establecemos el tamaño minimo que puede tomar nuestra ventana
+        
+        //this.getContentPane().setBackground(Color.blue);//Establecemos un color a la ventana
+        iniciarComponentes();
         setDefaultCloseOperation(EXIT_ON_CLOSE);//cuando cerremos la ventana el programa tambien finaliza
+    }
+    
+    private void iniciarComponentes(){
+        JPanel panel = new JPanel();//Creacion del panel
+        panel.setBackground(Color.green);//establecemos el color del panel
+        this.getContentPane().add(panel);//agregamosd el panel a la ventana
+        
     }
 }
