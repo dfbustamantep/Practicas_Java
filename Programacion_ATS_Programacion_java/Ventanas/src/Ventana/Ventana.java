7e0700abb3fca6a797e38a79c8e190733ef9b97d
dfbustamantep <vtdanielf@gmail.com>
2023-08-24 23:11:29 -0500

    Posicion de la ventana setLocation,setBounds,setLoactionRelativeto Programacion ATS video 88

--- Programacion_ATS_Programacion_java/Ventanas/src/Ventana/Ventana.java
+++ Programacion_ATS_Programacion_java/Ventanas/src/Ventana/Ventana.java
@@ -7,7 +7,10 @@ public class Ventana extends JFrame{
     public Ventana(){
         //ancho,largo
         this.setSize(500,500);//establecemos el tamaño de la ventana
-        setDefaultCloseOperation(EXIT_ON_CLOSE);//cuando cerremos la ventana el programa tambien finaliza
         setTitle("El mejor titulo");//establecemos el titulo de la ventana
+        //setLocation(100,200);//Coordenada X Coordenada Y Establecemos la posicion inicla de la vantana
+        //setBounds(100, 200, 500, 500);//setLocation y setSize en un mismo metodo, primeros parametros las cooredenas  X y Y,los otors dos son el tamaño de la ventana
+        setLocationRelativeTo(null);//null establecemos en el centro de la pantalla
+        setDefaultCloseOperation(EXIT_ON_CLOSE);//cuando cerremos la ventana el programa tambien finaliza
     }
 }
