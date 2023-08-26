d8fc2e5ff669a1686ded8fbf30401bab278fe3b7
dfbustamantep <vtdanielf@gmail.com>
2023-08-25 19:12:28 -0500

    Eventos Oyente del raton (MouseListener) Programacion ATS video 104

--- Programacion_ATS_Programacion_java/Eventos/src/eventos/Ventana.java
+++ Programacion_ATS_Programacion_java/Eventos/src/eventos/Ventana.java
@@ -7,10 +7,13 @@ package eventos;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
@@ -22,6 +25,7 @@ public class Ventana extends JFrame{
     private JLabel saludo;
     private JTextField cajaTexto;
     private JButton boton;
+    private JTextArea areaTexto;
     
     public Ventana(){
         setBounds(50,50,500,500);
@@ -35,7 +39,7 @@ public class Ventana extends JFrame{
     private void iniciarComponentes(){
         colocarPanel();
         colocarEtiqueta();
-        colocarCajaDeTexto();
+        colocarAreaDeTexto();
         colocarBoton();
         
     }
@@ -47,40 +51,83 @@ public class Ventana extends JFrame{
     }
     
     private void colocarEtiqueta(){
-        JLabel etiqueta = new JLabel ("Ingrese su nombre");
+        /*JLabel etiqueta = new JLabel ("Ingrese su nombre");
         etiqueta.setBounds(30, 10,200,30);
         etiqueta.setFont(new Font("Comic Sans MS",0,20));
-        panel.add(etiqueta);
+        panel.add(etiqueta);*/
     }
     
-    private void colocarCajaDeTexto(){
-        cajaTexto = new JTextField();
+    private void colocarAreaDeTexto(){
+        /*cajaTexto = new JTextField();
         cajaTexto.setBounds(30,50,300,30);
-        panel.add(cajaTexto);
+        panel.add(cajaTexto);*/
+        
+        areaTexto = new JTextArea();
+        areaTexto.setBounds(20,20,200,300);
+        panel.add(areaTexto);
     }
     
     private void colocarBoton(){
         boton = new JButton("¡Pulsa aqui!");
-        boton.setBounds(150,100,150,40);
+        boton.setBounds(150,350,150,40);
         boton.setFont(new Font("Comics Sans MS",0,15));
         panel.add(boton);
         
         saludo = new JLabel();
-        saludo.setBounds(50,200,300,40);
+        saludo.setBounds(50,400,300,40);
         saludo.setFont(new Font("Comics Sans MS",0,15));
         panel.add(saludo);
         
+        //eventoOyenteDeAccion();
+        eventoOyenteDeRaton();
+                
+    }
+    
+    private void eventoOyenteDeAccion(){
         //Agregamos el evento de tipo ActionListener
         ActionListener oyenteDeAccion;
         oyenteDeAccion = new ActionListener() {
             
             @Override
             public void actionPerformed(ActionEvent e) {
-                saludo.setText("Hola "+cajaTexto.getText());
+                saludo.setText("Hola "+areaTexto.getText());
                /* String a =cajaTexto.getText();
                 System.out.println(a);*/
             }
         };
         boton.addActionListener(oyenteDeAccion);
+    }
+    
+    private void eventoOyenteDeRaton(){
+        //agregando oyente de raton 
+        MouseListener oyenteDeRaton;
+        oyenteDeRaton = new MouseListener() {
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {//presionar y solatar el boton dentro del boton
+                areaTexto.append("Mouse clicked\n");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {//presionar el boton
+                areaTexto.append("Mouse pressed\n");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {//presionar y soltar boton
+                areaTexto.append("Mouse relessed\n");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                areaTexto.append("Mouse entered\n");//meter el cursor en el boton
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                areaTexto.append("Mouse exited\n");//quitar el cursosr del boton
+            }
+        };
+        boton.addMouseListener(oyenteDeRaton);
     }
 }
